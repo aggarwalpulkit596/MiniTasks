1467f7a83de058756c2a68651bfe49901c954cf0
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2018-09-04 02:58:24 +0530

    Initial Commit

---
+++ MiniTasks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniTasks
+//
+//  Created by Pulkit Aggarwal on 04/09/18.
+//  Copyright © 2018 Maxx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
